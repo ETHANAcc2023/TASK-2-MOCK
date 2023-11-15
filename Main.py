20f61cb41199629379d9f1b5bab420946f52c58f
ETHANAcc2023 <ethan.hurkett.754@accesstomusic.ac.uk>
2023-11-15 15:53:23 +0000

    Test_Change

--- Main.py
+++ Main.py
@@ -26,6 +26,13 @@ class RegisterForm(FlaskForm):
     confirm_password = PasswordField("Confirm Password:",validators=[DataRequired(), EqualTo('confirm_password')])
     submit = SubmitField("submit")
 
+class SignInForm(FlaskForm):
+    email = StringField("enter Email:",validators=[DataRequired()])
+    username = StringField("enter username:",validators=[DataRequired()])
+    password = PasswordField("enter password:",validators=[DataRequired(), EqualTo('confirm_password')])
+    confirm_password = PasswordField("Confirm Password:",validators=[DataRequired(), EqualTo('confirm_password')])
+    submit = SubmitField("submit")
+
 class Answer(FlaskForm):
     answer = StringField("Answer:")
 
@@ -38,7 +45,7 @@ def homepage():
 
 @app.route('/sign_up', methods = ['GET','POST'])
 def sign_up_page():
-    form = RegisterForm()
+    form = SignInForm()
     if form.validate_on_submit():
         userEmail = Forms_users.query.filter_by(email=form.email.data).first()
         userName = Forms_users.query.filter_by(username=form.username.data).first()
@@ -57,7 +64,22 @@ def sign_up_page():
 
 @app.route('/sign_in', methods = ['GET','POST'])
 def sign_In_page():
-    return render_template('sign_in_page.html')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        userEmail = Forms_users.query.filter_by(email=form.email.data).first()
+        userName = Forms_users.query.filter_by(username=form.username.data).first()
+        if userName is None and userEmail is None:
+            user = Forms_users(username=form.username.data, email = form.email.data, password=form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+            flash("You aready have an account!")
+        session['name'] = form.username.data
+        form.username.data = ''
+        return redirect("/")
+    return render_template('sign_in_page.html', form=form, name=session.get('name'),known=session.get('known',False))
 
 @app.route('/profile', methods = ['GET','Post'])
 def Profile_page():
